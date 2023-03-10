b01ddb4dcddc692822250edfe7b52c856943ab8b
Alejandro Arnedo <89948420+ArnedoAlejandro@users.noreply.github.com>
2023-03-10 17:28:03 -0300

    Secon comit delet spinner

--- src/component/Spiner.jsx
+++ src/component/Spiner.jsx
@@ -1,19 +1,1 @@
 
-import "../styles/spiners.css"
-
-const Spiner = () => {
-  return (
-    
-    <div className="sk-chase">
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-        <div class="sk-chase-dot"></div>
-    </div>
-    
-  )
-}
-
-export default Spiner
